a25fcecf357796f9622d1fc58c4302cab78938bb
jaenny <hongryeon_21@naver.com>
2020-05-16 14:58:58 +0900

    login

--- mydiary/admin.py
+++ mydiary/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
+from .models import Content, Comment
 
 # Register your models here.
 
-from .models import Content
-
 admin.site.register(Content)
+admin.site.register(Comment)
